338964b828c6ab71b3abb476a4cc54593c901f25
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-01-18 21:57:20 +0300

    first commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by Berkay Emre Aslan on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
