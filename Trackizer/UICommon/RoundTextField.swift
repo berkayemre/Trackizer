b51be826c72c5e59669156c0b9416e1694676a4f
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-02-03 21:43:26 +0300

    Building SignInView and RoundedTF

---
+++ Trackizer/UICommon/RoundTextField.swift
@@ -0,0 +1,63 @@
+//
+//  RoundTextField.swift
+//  Trackizer
+//
+//  Created by Berkay Emre Aslan on 3.02.2025.
+//
+
+import SwiftUI
+
+struct RoundTextField: View {
+    @State var title: String = "Title"
+    @Binding var text: String
+    @State var keyboardType:  UIKeyboardType = .default
+    var textAlign:Alignment  = .leading
+    var isPassword: Bool = false
+    
+    var body: some View {
+        VStack{
+            Text(title)
+                .multilineTextAlignment(.leading)
+                .font(.customfont(.regular, fontSize: 14))
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: textAlign)
+                
+                .foregroundColor(.gray50)
+                .padding(.bottom, 4)
+            
+            
+            if(isPassword) {
+                SecureField("", text: $text)
+                    .padding(15)
+                    
+                    .frame(height: 48)
+                    .overlay {
+                        RoundedRectangle(cornerRadius:  15)
+                            .stroke(Color.gray70, lineWidth: 1)
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.gray60.opacity(0.05))
+                    .cornerRadius(15)
+            }else{
+                TextField("", text: $text)
+                    .padding(15)
+                    .keyboardType(keyboardType)
+                    .frame(height: 48)
+                    .overlay {
+                        RoundedRectangle(cornerRadius:  15)
+                            .stroke(Color.gray70, lineWidth: 1)
+                    }
+                    .foregroundColor(.white)
+                    .background(Color.gray60.opacity(0.05))
+                    .cornerRadius(15)
+            }
+            
+            
+            
+        }
+    }
+}
+
+
+#Preview {
+    RoundTextField(text: <#Binding<String>#>)
+}
