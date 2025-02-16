718bcb0a72aa555c179fba3841ebaa17223f172c
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-02-16 23:42:37 +0300

    Building InfiniteWeekView

---
+++ Trackizer/3rdParty/InfiniteWeekView/Models/Week.swift
@@ -0,0 +1,14 @@
+//
+//  Week.swift
+//  Trackizer
+//
+//  Created by Berkay Emre Aslan on 16.02.2025.
+//
+
+import Foundation
+
+struct Week {
+    let index: Int
+    let dates: [Date]
+    var referenceDate: Date
+}
