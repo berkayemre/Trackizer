3cbbdfc16593bd773dc788647acbbf5a42a838d5
Berkay Emre Aslan <emreaslan1177@gmail.com>
2025-02-18 23:49:59 +0300

    Building Enums

---
+++ Trackizer/3rdParty/InfiniteWeekView/Utils/Enums/TimeDirection.swift
@@ -0,0 +1,14 @@
+//
+//  TimeDirection.swift
+//  Trackizer
+//
+//  Created by Berkay Emre Aslan on 18.02.2025.
+//
+
+import Foundation
+
+enum TimeDirection {
+    case future
+    case past
+    case unknown
+}
